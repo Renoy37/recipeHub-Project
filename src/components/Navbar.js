41f1c7199f3369ce12f54a05b3abbccbe553bfcc
Robins <robinmgk077@gmail.com>
2024-02-13 18:44:25 +0300

    Completed Navbar UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,16 @@ import React from "react";
 function Navbar() {
   return (
     <div>
-      Navbar
-      <div>
-        <h3>Home</h3>
+      <div className="Nav-container">
+        <div className="nav-branding">
+          Recipe<span className="hub">HUB</span>
+        </div>
+
+        <ul className="nav-link-container">
+          <li className="nav-links">Home</li>
+          <li className="nav-links">Discover</li>
+          <li className="nav-links">AddYours</li>
+        </ul>
       </div>
     </div>
   );
