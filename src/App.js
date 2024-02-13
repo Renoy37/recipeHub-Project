b21e4c8d22fdb997e5847a344268f5bffc52a46b
Brian <briomwaura@gmail.com>
2024-02-13 15:53:22 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Discover from "./components/Discover";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Discover />
     </div>
   );
 }
