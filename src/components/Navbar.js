a85f4518c96682f4614f0b9a0836a68b23eac250
Robins <robinmgk077@gmail.com>
2024-02-13 18:23:13 +0300

    Added Navbar element

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <div>
+        <h3>Home</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
