8b762629c43e3c550b4ad3c44d7a7c5f6162324b
mujahidina <mujahidabdi5070@gmail.com>
2024-02-13 19:12:41 +0300

    created a form

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {use} from "react";
 
 function PostPage(){
+
+
+
+    function handlePost() {
+        fetch("#", { 
+          method: 'POST', 
+          mode: 'cors', 
+          body: JSON.stringify(jsonData)
+        })
+        
+      }
+
+
+
+
     return(
    <div className="post-page">
-      
+          <form>
+            <input/>
+          </form>
+
+          <div className="post-page-details">
+      <p>Share the love, share the flavor! Contribute your
+         favorite recipe and let's create a culinary masterpiece together. 
+         Your dish could be the next big hit on our platform – tantalize taste 
+         buds and inspire fellow foodies worldwide!</p>
+
+         <img src=""></img>
+          </div>
    </div>
     );
 }
