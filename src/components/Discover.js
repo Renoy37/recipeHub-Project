2f3fc151704ca0aa80bb713449afd3bbddbb799d
Brian <briomwaura@gmail.com>
2024-02-15 00:55:41 +0300

    added and styled

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -2,35 +2,22 @@ import Image1 from '../Assets/Untitled design (1).png'
 
 function Discover (){
     return(
-        <>
-        
-            <section>
-              <img src={Image1} />
-            </section>
-
-            <section>
-                <div>
-                    <h2>DISCOVER MORE RECIPES</h2>
-                </div>
-                <div>
-                    <p>
+      <div className="landingPage">
+            <div className="intro">
+            <div className="homeImage">
+                <img src={Image1} alt="pizza" />
+            </div>
+                <h1>DISCOVER MORE<span className="highlighted">COOK?</span></h1>
+                <p>
                         The classic burger is an all-time BBQ favourite! This super easy homemade beef burger recipe gives you delicious patties, packed with onions and herbs for extra flavour, that are perfect for topping with cheese, lettuce and tomato, and sandwiching between floury buns.  
                         See Method:
                         Serves 4
                         20 mins to prepare and 17 mins to cook
                         472 calories / serving
                     </p>
-                </div>
-            </section>
-            <section>
-                <div>
-                    <h2>SEARCH FOR RECIPES: </h2>
-                    <button placeholder='serch here'>Click Me</button>
-                    <button background-color='red' color='white'>Search</button>
-                </div>
-            </section>
+            </div>
 
-        </>
+        </div>
     );
 }
 
