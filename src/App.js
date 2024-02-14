f0b0bde9440c13ac5acdb1ca7878cdf8a273519f
Brian <briomwaura@gmail.com>
2024-02-14 18:04:57 +0300

    added style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Discover from "./components/Discover";
+import Discover from './components/Discover'
 
 function App() {
   return (
