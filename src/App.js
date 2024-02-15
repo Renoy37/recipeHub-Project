d1056adbbb5930751c480310cdb5e1fe3b593deb
Robins <robinmgk077@gmail.com>
2024-02-15 15:05:24 +0300

    Last nav update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
           <Route path="/home">
