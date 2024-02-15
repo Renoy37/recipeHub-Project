f8f583c5f22ec8cca2c5a821310552741535a829
Kelvin Masambu <kevohmasambu@gmail.com>
2024-02-15 08:56:47 +0300

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-social">
+          <h3>Follow Us</h3>
+          <ul className="social-links">
+            <li><a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a></li>
+            <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
+            <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 RecipeHUB. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
