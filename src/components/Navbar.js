cd4bf4cf2f66366bd0c850ea2f064d28c617f6d3
Robins <robinmgk077@gmail.com>
2024-02-15 13:07:07 +0300

    Updated Nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import chef from "../Assets/chef.jpeg";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="Nav-container">
         <div className="nav-branding">
+          <img src={chef} alt="chef" className="chef" />
           Recipe<span className="hub">HUB</span>
         </div>
 
         <ul className="nav-link-container">
           <li className="nav-links">
-            <Link to="/Home">Home</Link>
+            <Link to="/Home" className="navs">
+              Home
+            </Link>
           </li>
           <li className="nav-links">
-            <Link to="/Discover">Discover</Link>
+            <Link to="/Discover" className="navs">
+              Discover
+            </Link>
           </li>
           <li className="nav-links">
-            <Link to="/PostPage">Add Yours</Link>
+            <Link to="/PostPage" className="navs">
+              Add Yours
+            </Link>
           </li>
         </ul>
       </div>
